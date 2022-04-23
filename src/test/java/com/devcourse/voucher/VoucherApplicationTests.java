658b619faeb1c22fe8ed10cc3d669332fe811d44
Woogie Kim <mdir2kor@gmail.com>
2022-04-23 14:36:29 +0900

    초기설정 추가

---
+++ src/test/java/com/devcourse/voucher/VoucherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcourse.voucher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
